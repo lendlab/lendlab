abd5071f62ff3d15a0d6ac6189bb97273d2c48f8
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-09-08 04:02:15 -0300

    Identation changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {ChakraProvider} from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 
 import App from "./App";
 import theme from "./theme/index";
