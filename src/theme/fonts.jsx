eea1dc8cc8b796628b80a2d696cba4ea2eb3e3c2
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-09-08 04:05:03 -0300

    Deleting Sk-Modernist font

--- src/theme/fonts.jsx
+++ src/theme/fonts.jsx
@@ -1,27 +1,9 @@
 import React from "react";
-import {Global} from "@emotion/react";
+import { Global } from "@emotion/react";
 
 const Fonts = () => (
   <Global
     styles={`
-      @font-face {
-        font-family: 'Sk-Modernist';
-        font-style: normal;
-        font-weight: normal;
-        font-display: swap;
-        src: url('fonts/Sk-Modernist-Regular.woff2') format('woff2'), url('./fonts/Sk-Modernist-Regular.woff') format('woff');
-        unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-      }
-
-      @font-face {
-        font-family: 'Sk-Modernist';
-        font-style: normal;
-        font-weight: bold;
-        font-display: swap;
-        src: url('/fonts/Sk-Modernist-Bold.woff2') format('woff2'), url('./fonts/Sk-Modernist-Bold.woff') format('woff');
-        unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-      }
-
       @font-face {
         font-family: 'Basement Grotesque';
         font-style: normal;
