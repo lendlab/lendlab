3f05a24dd31f0d8316e66df59930c971482fa10d
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-09-08 04:04:20 -0300

    Redisigning all Landing

--- src/app/screens/Landing/Landing.jsx
+++ src/app/screens/Landing/Landing.jsx
@@ -1,42 +1,48 @@
-import {Button} from "@chakra-ui/button";
-import {Image} from "@chakra-ui/image";
-import {Heading, Stack, Text} from "@chakra-ui/layout";
+import { Button } from "@chakra-ui/button";
+import { Image } from "@chakra-ui/image";
+import { Stack } from "@chakra-ui/layout";
 import React from "react";
 
-import {landingData} from "~/app/constants/landing";
+import { landingData } from "~/app/constants/landing";
 import LandingLayout from "~/app/layouts/LandingLayout";
+import Heading from "~/ui/text/Heading";
+import Text from "~/ui/text/Text";
+import Bold from "~/ui/text/Bold";
 
 const Landing = () => {
+  const { main } = landingData;
+
   return (
     <LandingLayout>
-      <Stack as="main" direction="row" minH="80vh" w="full">
-        <Stack justifyContent="center" spacing={4} w="50%">
-          <Stack alignItems="center" direction="row">
-            <Image src={landingData.main.flag} />
-            <Text fontWeight="bold" paddingLeft={4}>
-              {landingData.main.institutionCount}
-            </Text>
-            <Text>{landingData.main.institutionChoice}</Text>
-          </Stack>
-          <Heading as="h1" fontSize="6xl">
-            {landingData.main.titleOne}
+      <Stack
+        alignItems="center"
+        as="main"
+        direction="column"
+        justifyContent="center"
+        minH="80vh"
+        paddingTop={8}
+        spacing={4}
+        w="full"
+      >
+        <Heading as="h1" fontSize="6xl" maxW="15em" textAlign="center">
+          Tú
+          <Heading
+            as="h2"
+            bgClip="text"
+            bgGradient="linear(to-l, lendlab.blue,#FFF)"
+            fontSize="6xl"
+          >
+            Institución
           </Heading>
-          <Heading fontSize="6xl">{landingData.main.titleTwo}</Heading>
-          <Text fontWeight="bold">{landingData.main.features}</Text>
-          <Stack spacing={0}>
-            <Text>{landingData.main.information}</Text>
-            <Stack direction="row" spacing={1}>
-              <Text>{landingData.main.joinNow}</Text>
-              <Text fontWeight="bold" marginTop={0} paddingTop={0}>
-                {landingData.main.price}
-              </Text>
-            </Stack>
-          </Stack>
-          <Button fontSize="xl" leftIcon={landingData.main.button.icon()} padding={6} w="70%">
-            {landingData.main.button.text}
-          </Button>
-        </Stack>
-        <Image maxH="80vh" objectFit="contain" src="/images/computerIllustration.svg" />
+          ahora en forma online
+        </Heading>
+        <Text textAlign="center" w="70%">
+          Te permitimos realizar los prestamos de tu institución. ¿Eres laboratorista?, olvidate del
+          lapiz y papel, gestiona ahora! <Bold>Gratuitamente</Bold>
+        </Text>
+        <Button fontSize={15} size="lg" variant="primary">
+          Empezar Ahora
+        </Button>
       </Stack>
       <Stack direction="row" spacing={28} w="full">
         <Image src="/images/mediumSizeCircle.svg" />
