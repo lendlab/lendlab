54bd982f8f70ef1867c9e1235b461fdfb7e01877
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-09-08 04:03:23 -0300

    Adding some props to Input component

--- src/ui/inputs/Input.jsx
+++ src/ui/inputs/Input.jsx
@@ -1,10 +1,16 @@
-import {Input as ChakraInput, InputGroup} from "@chakra-ui/input";
+import { Input as ChakraInput, InputGroup } from "@chakra-ui/input";
 import React from "react";
 
-const Input = ({children, w, ...props}) => {
+const Input = ({ children, w, ...props }) => {
   return (
     <InputGroup size="lg" w={w}>
-      <ChakraInput {...props} bg="lendlab.gray.input" variant="filled" />
+      <ChakraInput
+        {...props}
+        _placeholder={{ color: "lendlab.gray.300" }}
+        bg="lendlab.gray.input"
+        borderRadius="17"
+        variant="filled"
+      />
       {children}
     </InputGroup>
   );
