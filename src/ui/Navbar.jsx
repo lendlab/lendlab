2f897fe9f652daeccc820f4664f83d48141b0e15
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-09-08 04:05:38 -0300

    Redisigning all Navbar

--- src/ui/Navbar.jsx
+++ src/ui/Navbar.jsx
@@ -1,48 +1,50 @@
-import {Button} from "@chakra-ui/button";
+import { Button } from "@chakra-ui/button";
 import Icon from "@chakra-ui/icon";
-import {Container, Stack, Text} from "@chakra-ui/layout";
+import { Box, Container, Stack, Text } from "@chakra-ui/layout";
 import React from "react";
-import {Link, useLocation} from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
-import {landingData} from "~/app/constants/landing";
+import { landingData } from "~/app/constants/landing";
 
 import Logo from "./static/Logo";
 
 const Navbar = () => {
-  const {pathname} = useLocation();
+  const { pathname } = useLocation();
+  const { nav } = landingData;
 
   return (
-    <Container alignSelf="center" maxW="container.xl" overflow="hidden">
+    <Box as="header" w="100%">
       <Stack
         alignItems="center"
-        as="header"
-        borderBottom="1px"
-        borderColor="lendlab.gray.200"
+        as="nav"
         direction="row"
+        h="nav"
         justifyContent="space-between"
-        minH="10vh"
-        w="full"
+        paddingX={6}
+        w="100%"
       >
         <Logo isNotHome={pathname === "/inicio" ? false : true} />
         {pathname === "/inicio" && (
           <>
-            <Stack as="ul" direction="row" spacing={6}>
-              {landingData.nav.options.map((option, index) => (
-                <Stack key={index} as="li" direction="row">
+            <Stack alignItems="center" as="ul" direction="row" spacing={8}>
+              {nav.options.map((option, index) => (
+                <Stack key={index} alignItems="center" as="li" direction="row">
                   <Icon as={option.icon} />
-                  <Text color="lendlab.gray.300">{option.name}</Text>
+                  <Text color="lendlab.gray.300" fontSize={14} fontWeight="700">
+                    {option.name}
+                  </Text>
                 </Stack>
               ))}
             </Stack>
             <Link to="/login">
-              <Button leftIcon={landingData.nav.button.icon()} variant="solid">
-                {landingData.nav.button.text}
+              <Button leftIcon={nav.button.icon()} variant="primary">
+                {nav.button.text}
               </Button>
             </Link>
           </>
         )}
       </Stack>
-    </Container>
+    </Box>
   );
 };
 
