bb307a5f92bf33956b21e50f322c3eedea822af1
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-09-11 13:51:07 -0300

    Creating index for forms components exports

---
+++ src/components/forms/index.jsx
@@ -0,0 +1,5 @@
+import { FormControl } from "./FormControl";
+import { FormikStep } from "./FormikStep";
+import { FormikStepper } from "./FormikStepper";
+
+export { FormControl, FormikStep, FormikStepper };
