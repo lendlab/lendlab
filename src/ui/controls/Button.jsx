aa39e2fd8746fc7032fce1928641247343b775cc
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-09-11 13:49:15 -0300

    changing folder structure

--- src/ui/controls/Button.jsx
+++
@@ -1,8 +0,0 @@
-import { Button as ChakraButton } from "@chakra-ui/button";
-import React from "react";
-
-const Button = ({ text, ...props }) => {
-  return <Button {...props}>{text}</Button>;
-};
-
-export default Button;
