9e4e71973092a2c9a8fec9c9c60218dc213e5229
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-09-11 13:58:56 -0300

    Creating index for exports of landing sections components

---
+++ src/pages/landing/components/index.jsx
@@ -0,0 +1,7 @@
+import { Benefits } from "./Benefits";
+import { AboutUs } from "./AboutUs";
+import { Features } from "./Features";
+import { OurTeam } from "./OurTeam";
+import { Hero } from "./Hero";
+
+export { Benefits, AboutUs, Features, OurTeam, Hero };
