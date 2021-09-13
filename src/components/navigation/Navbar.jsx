c18287c33b10a1f37b95d1a1cc3aac08b7595b17
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-09-13 04:07:49 -0300

    Adding animations and changing paths

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -6,6 +6,9 @@ import { Link, useLocation } from "react-router-dom";
 import { HashLink } from "react-router-hash-link";
 import { NAV_DATA } from "@utils/constants/landing";
 import Logo from "@components/static/Logo";
+import { motion } from "framer-motion";
+
+const MotionBox = motion(Box);
 
 const Navbar = () => {
   const { pathname } = useLocation();
@@ -18,7 +21,18 @@ const Navbar = () => {
   window.addEventListener("scroll", changeShadow);
 
   return (
-    <Box as="header" bg="white" position="fixed" shadow={shadow ? "sm" : ""} w="100%" zIndex="100">
+    <MotionBox
+      animate={{ y: 0, opacity: 1 }}
+      as={motion.header}
+      bg="white"
+      initial={{ y: "-10vh", opacity: 0 }}
+      position="fixed"
+      shadow={shadow ? "sm" : ""}
+      style={{ backdropFilter: "blur(20px)", backgroundColor: "rgba(255, 255, 255, 0.25)" }}
+      transition={{ delay: 0.2, type: "spring", stiffness: 120 }}
+      w="100%"
+      zIndex="100"
+    >
       <Stack
         alignItems="center"
         as="nav"
@@ -28,8 +42,8 @@ const Navbar = () => {
         paddingX={6}
         w="100%"
       >
-        <Logo isNotHome={pathname === "/inicio" ? false : true} />
-        {pathname === "/inicio" && (
+        <Logo isNotHome={pathname === "/" ? false : true} />
+        {pathname === "/" && (
           <>
             <Stack alignItems="center" as="ul" direction="row" spacing={8}>
               {NAV_DATA.options.map((option, index) => (
@@ -51,7 +65,7 @@ const Navbar = () => {
           </>
         )}
       </Stack>
-    </Box>
+    </MotionBox>
   );
 };
 
