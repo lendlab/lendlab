7c8d30990a062cf2554062e31405351460c617d5
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-09-13 04:09:43 -0300

    Adding animations

--- src/components/static/WhiteCircle.jsx
+++ src/components/static/WhiteCircle.jsx
@@ -1,5 +1,9 @@
 import { Image } from "@chakra-ui/image";
+import { motion } from "framer-motion";
 import React from "react";
+import InView from "react-intersection-observer";
+
+const MotionImage = motion(Image);
 
 const WhiteCircle = ({ size, ...props }) => {
   const circles = {
@@ -8,7 +12,22 @@ const WhiteCircle = ({ size, ...props }) => {
     little: "/images/littleSizeCircle.svg",
   };
 
-  return <Image src={circles[size]} zIndex="-9999" {...props} />;
+  return (
+    <InView threshold={0}>
+      {({ ref, inView }) => (
+        <MotionImage
+          ref={ref}
+          animate={inView && { y: 0, opacity: 1 }}
+          as={motion.img}
+          initial={{ y: "-10vh", opacity: 0 }}
+          src={circles[size]}
+          transition={{ delay: 0.2, type: "spring", stiffness: 200 }}
+          zIndex="-9999"
+          {...props}
+        />
+      )}
+    </InView>
+  );
 };
 
 export default WhiteCircle;
