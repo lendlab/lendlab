aadc84e52db3b8c9790a544e40f55f019cabf018
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-09-18 08:13:48 -0300

    Adding Headroom

--- src/ui/components/Nav.jsx
+++ src/ui/components/Nav.jsx
@@ -5,62 +5,64 @@ import { HashLink } from "react-router-hash-link";
 import { NAV_DATA } from "@utils/constants/landing";
 import { motion } from "framer-motion";
 import { Logo, Box } from "@ui";
+import Headroom from "react-headroom";
 
 export const Nav = () => {
   const { pathname } = useLocation();
   const [shadow, setShadow] = useState(false);
 
-  const changeShadow = () => {
+  const handleScroll = () => {
     window.scrollY >= 80 ? setShadow(true) : setShadow(false);
   };
 
-  window.addEventListener("scroll", changeShadow);
+  window.addEventListener("scroll", handleScroll);
 
   return (
-    <Box
-      animate={{ y: 0, opacity: 1 }}
-      as={motion.header}
-      bg="white"
-      initial={{ y: "-10vh", opacity: 0 }}
-      position="fixed"
-      shadow={shadow ? "sm" : ""}
-      style={{ backdropFilter: "blur(20px)", backgroundColor: "rgba(255, 255, 255, 0.25)" }}
-      transition={{ delay: 0.2, type: "spring", stiffness: 120 }}
-      w="100%"
-      zIndex="100"
-    >
-      <Stack
-        alignItems="center"
-        as="nav"
-        direction="row"
-        h="nav"
-        justifyContent="space-between"
-        paddingX={6}
+    <Headroom>
+      <Box
+        animate={{ y: 0, opacity: 1 }}
+        as={motion.header}
+        bg="white"
+        initial={{ y: "-10vh", opacity: 0 }}
+        shadow={shadow ? "sm" : ""}
+        style={{ backdropFilter: "blur(20px)", backgroundColor: "rgba(255, 255, 255, 0.25)" }}
+        transition={{ delay: 0.2, type: "spring", stiffness: 120 }}
         w="100%"
+        zIndex="100"
       >
-        <Logo isNotHome={pathname === "/" ? false : true} />
-        {pathname === "/" && (
-          <>
-            <Stack alignItems="center" as="ul" direction="row" spacing={8}>
-              {NAV_DATA.options.map((option, index) => (
-                <HashLink key={index} to={option.link}>
-                  <Stack alignItems="center" direction="row">
-                    <Icon as={option.icon} />
-                    <Text color="lendlab.gray.300" fontSize={14} fontWeight="700">
-                      {option.name}
-                    </Text>
-                  </Stack>
-                </HashLink>
-              ))}
-            </Stack>
-            <Link to="/login">
-              <Button leftIcon={NAV_DATA.button.icon()} variant="primary">
-                {NAV_DATA.button.text}
-              </Button>
-            </Link>
-          </>
-        )}
-      </Stack>
-    </Box>
+        <Stack
+          alignItems="center"
+          as="nav"
+          direction="row"
+          h="nav"
+          justifyContent="space-between"
+          paddingX={6}
+          w="100%"
+        >
+          <Logo isNotHome={pathname === "/" ? false : true} />
+          {pathname === "/" && (
+            <>
+              <Stack alignItems="center" as="ul" direction="row" spacing={8}>
+                {NAV_DATA.options.map((option, index) => (
+                  <HashLink key={index} to={option.link}>
+                    <Stack alignItems="center" direction="row">
+                      <Icon as={option.icon} />
+                      <Text color="lendlab.gray.300" fontSize={14} fontWeight="700">
+                        {option.name}
+                      </Text>
+                    </Stack>
+                  </HashLink>
+                ))}
+              </Stack>
+              <Link to="/login">
+                <Button leftIcon={NAV_DATA.button.icon()} variant="primary">
+                  {NAV_DATA.button.text}
+                </Button>
+              </Link>
+            </>
+          )}
+        </Stack>
+      </Box>
+    </Headroom>
   );
 };
