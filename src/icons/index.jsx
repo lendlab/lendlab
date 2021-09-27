eeded74430908091c1c6d812e96e2c521cc610dd
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-09-26 21:07:51 -0300

    Export added icons

--- src/icons/index.jsx
+++ src/icons/index.jsx
@@ -2,6 +2,7 @@ import { Arrow } from "./Arrow";
 import { BriefCase } from "./Briefcase";
 import { Bucket } from "./Bucket";
 import { Card } from "./Card";
+import { Cart } from "./Cart";
 import { Cup } from "./Cup";
 import { DashboardIcon } from "./DashboardIcon";
 import { Edit } from "./Edit";
@@ -16,6 +17,7 @@ import { LoginIcon } from "./LoginIcon";
 import { LogoutIcon } from "./LogoutIcon";
 import { Messages } from "./Messages";
 import { NewPrestamo } from "./NewPrestamo";
+import { Notification } from "./Notification";
 import { People } from "./People";
 import { Prestamo } from "./Prestamo";
 import { ProfileUser2 } from "./ProfileUser2";
@@ -54,4 +56,6 @@ export {
   Table,
   Grid,
   List,
+  Notification,
+  Cart,
 };
