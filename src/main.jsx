ca6ff6332a615da9c756ba7940e3325b576393a9
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-09-27 05:57:57 -0300

    Add ApolloProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { ChakraProvider } from "@chakra-ui/react";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 import "@babel/polyfill";
 import App from "./app";
@@ -9,10 +10,17 @@ import "@fontsource/manrope";
 import "@fontsource/archivo";
 import Fonts from "./theme/fonts";
 
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache(),
+});
+
 ReactDOM.render(
-  <ChakraProvider theme={theme}>
-    <Fonts />
-    <App />
-  </ChakraProvider>,
+  <ApolloProvider client={client}>
+    <ChakraProvider theme={theme}>
+      <Fonts />
+      <App />
+    </ChakraProvider>
+  </ApolloProvider>,
   document.getElementById("root")
 );
