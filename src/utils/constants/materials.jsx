989fa544d2f3a5dc26a8d6f5cb4352e0f76f4dc5
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-09-26 21:16:45 -0300

    Add materials

---
+++ src/utils/constants/materials.jsx
@@ -0,0 +1,43 @@
+export const materials = [
+  {
+    nombre: "Ceibalita",
+    src: "https://www.lr21.com.uy/wp-content/uploads/2020/03/plan-ceibal.jpg",
+    desc: "Lorem ipsum dolor sit amet, consectetur . Duis aute irure dolor in reprehenderit",
+    state: "Buen",
+    category: "Laptops",
+  },
+  {
+    nombre: "Auricular",
+    src: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1d/Earphones_BW_2011-12-10_15-49-08.JPG/1200px-Earphones_BW_2011-12-10_15-49-08.JPG",
+    desc: "Lorem ipsum dolor sit amet, consectetur . Duis aute irure dolor in reprehenderit",
+    state: "Mal",
+    category: "Auriculares",
+  },
+  {
+    nombre: "Cargador",
+    src: "https://www.digitaloutlet.com.uy/imgs/productos/productos34_3721.jpg",
+    desc: "Lorem ipsum dolor sit amet, consectetur . Duis aute irure dolor in reprehenderit",
+    state: "Regular",
+    category: "Cargadores",
+  },
+  {
+    nombre: "GOD ceibalita",
+    src: "https://icdn.dtcn.com/image/digitaltrends_es/185-xps-13-laptop-resized-2.jpg",
+    desc: "Lorem ipsum dolor sit amet, consectetur . Duis aute irure dolor in reprehenderit",
+    category: "Laptops",
+  },
+  {
+    nombre: "Martillo",
+    src: "https://geant.vteximg.com.br/arquivos/ids/268992-1000-1000/478372.jpg?v=637522818658200000",
+    desc: "Lorem ipsum dolor sit amet, consectetur . Duis aute irure dolor in reprehenderit",
+    state: "Buen",
+    category: "Herramientas",
+  },
+  {
+    nombre: "Ceibalita",
+    src: "https://www.lr21.com.uy/wp-content/uploads/2020/03/plan-ceibal.jpg",
+    desc: "Lorem ipsum dolor sit amet, consectetur . Duis aute irure dolor in reprehenderit",
+    state: "Buen",
+    category: "Laptops",
+  },
+];
