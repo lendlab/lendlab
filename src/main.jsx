a1b918791e7016cc7e5987e56b01da83db52ea96
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-09-26 21:06:16 -0300

    Import the new fonts

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import { ChakraProvider } from "@chakra-ui/react";
 import "@babel/polyfill";
 import App from "./app";
 import theme from "./theme/index";
+import "@fontsource/manrope";
+import "@fontsource/archivo";
 import Fonts from "./theme/fonts";
 
 ReactDOM.render(
