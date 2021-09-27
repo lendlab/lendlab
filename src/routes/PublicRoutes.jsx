82dcb3554739c8c596259e2bc9996251cca0fbc2
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-09-26 21:12:51 -0300

    Add user as prop for PublicRoutes

--- src/routes/PublicRoutes.jsx
+++ src/routes/PublicRoutes.jsx
@@ -2,16 +2,24 @@ import React from "react";
 import { Redirect, Route } from "react-router-dom";
 import PropTypes from "prop-types";
 
-export const PublicRoutes = ({ isAuthenticated, component: Component, ...rest }) => {
+const PublicRoutes = ({ isAuthenticated, user, component: Component, ...rest }) => {
   return (
     <Route
       {...rest}
       component={(props) =>
-        !isAuthenticated ? <Component {...props} /> : <Redirect to="/app/resumen" />
+        !isAuthenticated ? (
+          <Component {...props} />
+        ) : user == "Laboratorista" ? (
+          <Redirect to="/app/prestamos" />
+        ) : (
+          <Redirect to="/app/home" />
+        )
       }
     />
   );
 };
+
+export default PublicRoutes;
 
 PublicRoutes.propTypes = {
   isAuthenticated: PropTypes.bool.isRequired,
