12f4126ced282831e866cbf9dd5d36e58170cca4
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-09-26 21:11:58 -0300

    Wrap LabRouter inside CartProvider

--- src/routes/LabRouter.jsx
+++ src/routes/LabRouter.jsx
@@ -6,9 +6,11 @@ import LabReservesPage from "@pages/lab/reserves";
 import LabMaterialsPage from "@pages/lab/materials";
 import LabUsersPage from "@pages/lab/users";
 
+import { CartProvider } from "../context/CartProvider";
+
 export const LabRouter = () => {
   return (
-    <>
+    <CartProvider>
       <Sidebar />
       <Switch>
         <Route component={LabLendsPage} path="/app/prestamos" />
@@ -17,6 +19,6 @@ export const LabRouter = () => {
         <Route component={LabUsersPage} path="/app/usuarios" />
         <Redirect to="/app/prestamos" />
       </Switch>
-    </>
+    </CartProvider>
   );
 };
