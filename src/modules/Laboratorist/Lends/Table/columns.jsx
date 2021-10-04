de8963664c33ffd1dc721333d99244be4635ae9e
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-10-04 05:38:51 -0300

    Add table columns

---
+++ src/modules/Laboratorist/Lends/Table/columns.jsx
@@ -0,0 +1,30 @@
+export const COLUMNS = [
+  {
+    Header: "ID",
+    accessor: "lendId",
+  },
+  {
+    Header: "Usuario",
+    accessor: "user",
+  },
+  {
+    Header: "Laboratorista",
+    accessor: "laboratorist",
+  },
+  {
+    Header: "Tipo",
+    accessor: "type",
+  },
+  {
+    Header: "Reserva",
+    accessor: "reserveId",
+  },
+  {
+    Header: "Plazo",
+    accessor: "term",
+  },
+  {
+    Header: "Devolución",
+    accessor: "devolution",
+  },
+];
