b6a9d345387d26760dfec53488e6a1ec126f277d
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-10-04 05:42:45 -0300

    Add reset function with fade animation

--- src/ui/components/Input.jsx
+++ src/ui/components/Input.jsx
@@ -1,10 +1,45 @@
-import { Input as ChakraInput, InputGroup } from "@chakra-ui/input";
+import {
+  Input as ChakraInput,
+  InputRightElement,
+  InputGroup,
+  Button,
+  Fade,
+} from "@chakra-ui/react";
 import React from "react";
 
-export const Input = ({ children, w, maxW, ...props }) => {
+export const Input = ({
+  children,
+  w,
+  maxW,
+  setValue,
+  isGlobalFilter,
+  setGlobalFilter,
+  ...props
+}) => {
   return (
     <InputGroup maxW={maxW} size="lg" w={w}>
       {children}
+      <Fade in={props.value != ""} initialScale={0.1} unmountOnExit={true}>
+        <InputRightElement
+          children={
+            <Button
+              aria-label="clear search"
+              color="black"
+              size="sm"
+              variant="ghost"
+              onClick={() => {
+                setValue("");
+                {
+                  isGlobalFilter && setGlobalFilter("");
+                }
+              }}
+            >
+              x
+            </Button>
+          }
+        />
+      </Fade>
+
       <ChakraInput
         {...props}
         _placeholder={{ color: "lendlab.gray.300", fontSize: "13px" }}
