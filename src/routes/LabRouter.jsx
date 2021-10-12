e4d2c32c71d593bc5bb700f9baa3b1240657a624
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-10-12 06:23:51 -0300

    Add Material and Lend routes

--- src/routes/LabRouter.jsx
+++ src/routes/LabRouter.jsx
@@ -6,6 +6,8 @@ import LabReservesPage from "@pages/lab/reserves";
 import LabMaterialsPage from "@pages/lab/materials";
 import LabUsersPage from "@pages/lab/users";
 import Summary from "@pages/lab/summary";
+import { Lend } from "@modules/Laboratorist/Lends/Lend";
+import { Material } from "@modules/Laboratorist/Materials/Material";
 
 import { CartProvider } from "../context/CartProvider";
 
@@ -18,8 +20,10 @@ export const LabRouter = (props) => {
       <Switch>
         <Route component={Summary} path="/app/resumen" />
         <Route component={LabLendsPage} path="/app/prestamos" />
+        <Route component={Lend} path="/app/prestamo/:id" />
         <Route component={LabReservesPage} path="/app/reservas" />
         <Route component={LabMaterialsPage} path="/app/materiales" />
+        <Route component={Material} path="/app/material/:id" />
         <Route component={LabUsersPage} path="/app/usuarios" />
         <Redirect to="/app/resumen" />
       </Switch>
