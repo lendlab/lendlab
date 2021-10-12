b38c9ea74f7a1844d3d12a0061cfd38ff1e7ac31
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-10-12 06:19:09 -0300

    Add create_lend

---
+++ src/graphql/mutations/lends.jsx
@@ -0,0 +1,17 @@
+import { gql } from "@apollo/client";
+
+export const CREATE_LEND = gql`
+  mutation CreateLendMutation($data: LendInput!) {
+    createLend(data: $data) {
+      id_lend
+      fecha_vencimiento
+      fecha_devolucion
+      fecha_hora_presta
+      reservation {
+        id_reserva
+        fecha_hora
+        finalizada
+      }
+    }
+  }
+`;
