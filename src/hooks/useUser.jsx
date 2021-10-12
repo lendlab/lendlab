825fb5506011fd20730f29a29f04d37b1fa5c55a
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-10-12 06:20:24 -0300

    Add reset function and the ability to filter with cedula too

--- src/hooks/useUser.jsx
+++ src/hooks/useUser.jsx
@@ -2,15 +2,19 @@ import React, { useEffect } from "react";
 
 export const useUser = (Users) => {
   const [usersFiltered, setUsersFiltered] = React.useState([]);
-  const [userSelected, setUserSelected] = React.useState("");
+  const [userSelected, setUserSelected] = React.useState({});
   const [user, setUser] = React.useState("");
 
   const handleUserChange = ({ target }) => {
     setUser(target.value);
   };
 
+  const handleReset = () => {
+    setUser("");
+  };
+
   const handleUserSelected = (user) => {
-    setUserSelected(user.nombre);
+    setUserSelected(user);
   };
 
   const handleUsersSelected = (user) => {
@@ -20,12 +24,16 @@ export const useUser = (Users) => {
   useEffect(() => {
     setUsersFiltered(
       Users.filter((userEl) => {
-        return userEl.nombre.toLowerCase().includes(user.toLowerCase());
+        return (
+          userEl.nombre.toLowerCase().includes(user.toLowerCase()) ||
+          userEl.cedula.toString().includes(user)
+        );
       })
     );
   }, [user]);
 
   return {
+    handleReset,
     user,
     usersFiltered,
     userSelected,
