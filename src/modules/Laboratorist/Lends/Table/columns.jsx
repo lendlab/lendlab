70a60a4980bc1884050b254342b413cb5d133692
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-10-14 06:16:56 -0300

    Add all lends table columns

--- src/modules/Laboratorist/Lends/Table/columns.jsx
+++ src/modules/Laboratorist/Lends/Table/columns.jsx
@@ -1,30 +1,68 @@
+import { Badge } from "@chakra-ui/layout";
+import { Tooltip } from "@chakra-ui/tooltip";
+import moment from "moment";
+import React from "react";
+
 export const COLUMNS = [
   {
     Header: "ID",
-    accessor: "lendId",
+    accessor: "id_lend",
   },
   {
-    Header: "Usuario",
-    accessor: "user",
-  },
-  {
-    Header: "Laboratorista",
-    accessor: "laboratorist",
-  },
-  {
-    Header: "Tipo",
-    accessor: "type",
-  },
-  {
-    Header: "Reserva",
-    accessor: "reserveId",
+    Header: "Fecha y hora",
+    accessor: "fecha_hora_presta",
+    Cell({ row }) {
+      const sqlDate = parseInt(row.original.fecha_hora_presta);
+
+      const date = moment(sqlDate).format("Do [de] MMMM [del] YYYY [a las] H:mm");
+
+      return (
+        <Tooltip aria-label={moment(sqlDate).fromNow()} label={moment(sqlDate).fromNow()}>
+          {date}
+        </Tooltip>
+      );
+    },
   },
   {
     Header: "Plazo",
-    accessor: "term",
+    accessor: "fecha_vencimiento",
+    Cell({ row }) {
+      const sqlDate = parseInt(row.original.fecha_vencimiento);
+
+      const date = moment(sqlDate).format("Do [de] MMMM [del] YYYY [a las] H:mm");
+
+      const fecha_inicio = moment(parseInt(row.original.fecha_hora_presta));
+
+      return (
+        <>
+          hasta el{" "}
+          <Tooltip aria-label={fecha_inicio.to(sqlDate)} label={fecha_inicio.to(sqlDate)}>
+            {date}
+          </Tooltip>
+        </>
+      );
+    },
   },
   {
-    Header: "Devolución",
-    accessor: "devolution",
+    Header: "Devolucion",
+    accessor: "fecha_devolucion",
+    Cell({ row }) {
+      if (row.original.fecha_devolucion == null)
+        return <Badge colorScheme="yellow">NO DEVUELTO</Badge>;
+
+      const sqlDate = parseInt(row.original.fecha_devolucion);
+
+      const date = moment(sqlDate).format("Do [de] MMMM [del] YYYY [a las] H:mm");
+
+      return (
+        <Tooltip aria-label={moment(sqlDate).fromNow()} label={moment(sqlDate).fromNow()}>
+          {date}
+        </Tooltip>
+      );
+    },
+  },
+  {
+    Header: "Reserva",
+    accessor: "reservation.id_reserva",
   },
 ];
