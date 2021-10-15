2adee89712d993c03e205f6f9e7cc514913e0531
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-10-15 00:59:20 -0300

    Add spanish lang for moment

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import moment from "moment";
+import es from "moment/locale/es";
 
 import { Router } from "./routes/Router";
+moment.locale("es", es);
 
 const App = () => {
   return <Router />;
