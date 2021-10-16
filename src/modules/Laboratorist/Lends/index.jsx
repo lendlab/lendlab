cdae3bf14bb365ff8d85930dc22a347216c7cfe8
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-10-16 07:20:07 -0300

    Add Layout

--- src/modules/Laboratorist/Lends/index.jsx
+++ src/modules/Laboratorist/Lends/index.jsx
@@ -18,6 +18,8 @@ import {
 import { useDisclosure } from "@chakra-ui/hooks";
 import { useUser } from "@hooks/useUser";
 
+import { LendsPageLayout } from "../../layouts/Laboratorist/LendsPageLayout";
+
 import { AllLends } from "./AllLends";
 
 export const LabLendsPage = () => {
@@ -62,133 +64,135 @@ export const LabLendsPage = () => {
   } = useUser(Users);
 
   return (
-    <SectionInfo
-      button={
-        <Button leftIcon={<NewPrestamo />} variant="primary" onClick={onDrawerOpen}>
-          Nuevo Préstamo
-        </Button>
-      }
-      description="En la sección de prestamos, puedes revisar y administrar todos los prestamos con sus detalles. Puedes ver, editar y eliminar cualquier tipo de información como la reserva de todos los useres, ordenados por IDs, fechas, y tipo de prestamo. El acceso a esta área es limitada. Solo Laboratoristas pueden alcanzarla. Los cambios que hagas son irreversibles."
-      icon={<Prestamo size="big" />}
-      title="Prestamos"
-    >
-      <TabList bg="lendlab.gray.100" paddingTop={8} paddingX={{ base: 0, md: 18 }}>
-        <Tab>
-          <Icon as={Prestamo} h="none" marginRight={2} w="none" /> TODOS
-        </Tab>
-        <Tab>FINALIZADOS</Tab>
-        <Tab>SIN DEVOLVER</Tab>
-        <Tab>DEVUELTOS</Tab>
-      </TabList>
-      <TabPanels bg="white">
-        <TabPanel paddingX={{ base: 0, md: 18 }}>
-          <AllLends />
+    <LendsPageLayout>
+      <SectionInfo
+        button={
+          <Button leftIcon={<NewPrestamo />} variant="primary" onClick={onDrawerOpen}>
+            Nuevo Préstamo
+          </Button>
+        }
+        description="En la sección de prestamos, puedes revisar y administrar todos los prestamos con sus detalles. Puedes ver, editar y eliminar cualquier tipo de información como la reserva de todos los useres, ordenados por IDs, fechas, y tipo de prestamo. El acceso a esta área es limitada. Solo Laboratoristas pueden alcanzarla. Los cambios que hagas son irreversibles."
+        icon={<Prestamo size="big" />}
+        title="Prestamos"
+      >
+        <TabList bg="lendlab.gray.100" paddingTop={8} paddingX={{ base: 0, md: 18 }}>
+          <Tab>
+            <Icon as={Prestamo} h="none" marginRight={2} w="none" /> TODOS
+          </Tab>
+          <Tab>FINALIZADOS</Tab>
+          <Tab>SIN DEVOLVER</Tab>
+          <Tab>DEVUELTOS</Tab>
+        </TabList>
+        <TabPanels bg="white">
+          <TabPanel paddingX={{ base: 0, md: 18 }}>
+            <AllLends />
 
-          <LendsDrawer
-            btnRef={btnRef}
-            isDrawerOpen={isDrawerOpen}
-            userButton={
-              <Button
-                bg="lendlab.gray.100"
-                borderRadius="14px"
-                color={userSelected == "" ? "lendlab.gray.300" : "lendlab.black"}
-                display="block"
-                fontSize="13px"
-                fontWeight="normal"
-                size="lg"
-                textAlign="left"
-                onClick={onModalOpen}
-              >
-                {Object.keys(userSelected).length === 0 ? "Buscar usuario" : userSelected.nombre}
-              </Button>
-            }
-            userSelected={userSelected}
-            onDrawerClose={onDrawerClose}
-          />
-          <Modal isOpen={isModalOpen} onClose={onModalClose}>
-            <ModalOverlay />
-            <ModalContent>
-              <ModalHeader>
-                <Input
-                  notWithFormik
-                  name="user"
-                  placeholder="Buscar Usuario"
-                  value={user}
-                  onChange={handleUserChange}
-                  onResetClick={handleReset}
-                />
-              </ModalHeader>
-              <ModalBody>
-                <Stack spacing={4}>
-                  {user == ""
-                    ? Users.slice(0, 3).map((user, index) => (
-                        <Stack
-                          key={index}
-                          cursor="pointer"
-                          direction="row"
-                          spacing={4}
-                          onClick={() => {
-                            handleUserSelected(user);
-                            onModalClose();
-                          }}
-                        >
-                          <Image
-                            borderRadius="12px"
-                            boxSize="70px"
-                            fallbackSrc="/images/fallback.jpg"
-                            objectFit="cover"
-                            src={user.src}
-                          />
-                          <Stack justifyContent="center">
-                            <Text color="black" fontSize="3" fontWeight="bold" textAlign="left">
-                              {user.nombre}
-                            </Text>
-                            <Text fontSize="3" noOfLines={1} textAlign="left">
-                              {user.cedula}
-                            </Text>
+            <LendsDrawer
+              btnRef={btnRef}
+              isDrawerOpen={isDrawerOpen}
+              userButton={
+                <Button
+                  bg="lendlab.gray.100"
+                  borderRadius="14px"
+                  color={userSelected == "" ? "lendlab.gray.300" : "lendlab.black"}
+                  display="block"
+                  fontSize="13px"
+                  fontWeight="normal"
+                  size="lg"
+                  textAlign="left"
+                  onClick={onModalOpen}
+                >
+                  {Object.keys(userSelected).length === 0 ? "Buscar usuario" : userSelected.nombre}
+                </Button>
+              }
+              userSelected={userSelected}
+              onDrawerClose={onDrawerClose}
+            />
+            <Modal isOpen={isModalOpen} onClose={onModalClose}>
+              <ModalOverlay />
+              <ModalContent>
+                <ModalHeader>
+                  <Input
+                    notWithFormik
+                    name="user"
+                    placeholder="Buscar Usuario"
+                    value={user}
+                    onChange={handleUserChange}
+                    onResetClick={handleReset}
+                  />
+                </ModalHeader>
+                <ModalBody>
+                  <Stack spacing={4}>
+                    {user == ""
+                      ? Users.slice(0, 3).map((user, index) => (
+                          <Stack
+                            key={index}
+                            cursor="pointer"
+                            direction="row"
+                            spacing={4}
+                            onClick={() => {
+                              handleUserSelected(user);
+                              onModalClose();
+                            }}
+                          >
+                            <Image
+                              borderRadius="12px"
+                              boxSize="70px"
+                              fallbackSrc="/images/fallback.jpg"
+                              objectFit="cover"
+                              src={user.src}
+                            />
+                            <Stack justifyContent="center">
+                              <Text color="black" fontSize="3" fontWeight="bold" textAlign="left">
+                                {user.nombre}
+                              </Text>
+                              <Text fontSize="3" noOfLines={1} textAlign="left">
+                                {user.cedula}
+                              </Text>
+                            </Stack>
                           </Stack>
-                        </Stack>
-                      ))
-                    : usersFiltered.map((user, index) => (
-                        <Stack
-                          key={index}
-                          cursor="pointer"
-                          direction="row"
-                          spacing={4}
-                          onClick={() => {
-                            handleUserSelected(user);
-                            onModalClose();
-                          }}
-                        >
-                          <Image
-                            borderRadius="12px"
-                            boxSize="70px"
-                            fallbackSrc="/images/fallback.jpg"
-                            objectFit="cover"
-                            src={user.src}
-                          />
-                          <Stack justifyContent="center">
-                            <Text color="black" fontSize="3" fontWeight="bold" textAlign="left">
-                              {user.nombre}
-                            </Text>
-                            <Text fontSize="3" noOfLines={1} textAlign="left">
-                              {user.cedula}
-                            </Text>
+                        ))
+                      : usersFiltered.map((user, index) => (
+                          <Stack
+                            key={index}
+                            cursor="pointer"
+                            direction="row"
+                            spacing={4}
+                            onClick={() => {
+                              handleUserSelected(user);
+                              onModalClose();
+                            }}
+                          >
+                            <Image
+                              borderRadius="12px"
+                              boxSize="70px"
+                              fallbackSrc="/images/fallback.jpg"
+                              objectFit="cover"
+                              src={user.src}
+                            />
+                            <Stack justifyContent="center">
+                              <Text color="black" fontSize="3" fontWeight="bold" textAlign="left">
+                                {user.nombre}
+                              </Text>
+                              <Text fontSize="3" noOfLines={1} textAlign="left">
+                                {user.cedula}
+                              </Text>
+                            </Stack>
                           </Stack>
-                        </Stack>
-                      ))}
-                </Stack>
-              </ModalBody>
-            </ModalContent>
-          </Modal>
-        </TabPanel>
-        <TabPanel>
-          <p>2</p>
-        </TabPanel>
-        <TabPanel>
-          <p>3</p>
-        </TabPanel>
-      </TabPanels>
-    </SectionInfo>
+                        ))}
+                  </Stack>
+                </ModalBody>
+              </ModalContent>
+            </Modal>
+          </TabPanel>
+          <TabPanel>
+            <p>2</p>
+          </TabPanel>
+          <TabPanel>
+            <p>3</p>
+          </TabPanel>
+        </TabPanels>
+      </SectionInfo>
+    </LendsPageLayout>
   );
 };
