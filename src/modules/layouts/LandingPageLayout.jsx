fcf6996b7db9b28dab79876e998392c6e7205ce7
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-10-16 07:23:09 -0300

    Add LandingPageLayout

--- src/modules/layouts/LandingPageLayout.jsx
+++ src/modules/layouts/LandingPageLayout.jsx
@@ -6,7 +6,19 @@ export const LandingPageLayout = ({ children }) => {
   return (
     <>
       <Helmet>
-        <title>lendlab | inicio</title>
+        <title>inicio | lendlabç</title>
+        <meta
+          content="Administra, gestiona y realiza los prestamso de tu institución, ahora en forma online con LendLab."
+          name="description"
+        />
+        <meta
+          content="Presentar a los usuarios la aplicación Lendlab, a forma de LandingPage"
+          name="subject"
+        />
+        <meta
+          content="lendlab, landingpage, inicio, prestamos, institucion, uruguay, escuelas, instituciones, materiales, administracion, gestion"
+          name="keywords"
+        />
       </Helmet>
       <Box overflow="hidden">{children}</Box>
     </>
