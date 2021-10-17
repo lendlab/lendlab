91d0c37a0bbb76e87dae1ca831635e6ffec26deb
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-10-17 20:31:19 -0300

    Add UserProvider and ApolloConsumer

--- src/routes/LabRouter.jsx
+++ src/routes/LabRouter.jsx
@@ -8,25 +8,33 @@ import LabUsersPage from "@pages/lab/users";
 import Summary from "@pages/lab/summary";
 import { Lend } from "@modules/Laboratorist/Lends/Lend";
 import { Material } from "@modules/Laboratorist/Materials/Material";
+import { ApolloConsumer } from "@apollo/client";
 
 import { CartProvider } from "../context/CartProvider";
+import { UserProvider } from "../context/UserProvider";
 
 export const LabRouter = (props) => {
   localStorage.setItem("lastPath", props.location.pathname);
 
   return (
-    <CartProvider>
-      <Sidebar />
-      <Switch>
-        <Route component={Summary} path="/app/resumen" />
-        <Route component={LabLendsPage} path="/app/prestamos" />
-        <Route component={Lend} path="/app/prestamo/:id" />
-        <Route component={LabReservesPage} path="/app/reservas" />
-        <Route component={LabMaterialsPage} path="/app/materiales" />
-        <Route component={Material} path="/app/material/:id" />
-        <Route component={LabUsersPage} path="/app/usuarios" />
-        <Redirect to="/app/resumen" />
-      </Switch>
-    </CartProvider>
+    <ApolloConsumer>
+      {(client) => (
+        <CartProvider client={client}>
+          <UserProvider>
+            <Sidebar />
+            <Switch>
+              <Route component={Summary} path="/app/resumen" />
+              <Route component={LabLendsPage} path="/app/prestamos" />
+              <Route component={Lend} path="/app/prestamo/:id" />
+              <Route component={LabReservesPage} path="/app/reservas" />
+              <Route component={LabMaterialsPage} path="/app/materiales" />
+              <Route component={Material} path="/app/material/:id" />
+              <Route component={LabUsersPage} path="/app/usuarios" />
+              <Redirect to="/app/resumen" />
+            </Switch>
+          </UserProvider>
+        </CartProvider>
+      )}
+    </ApolloConsumer>
   );
 };
