cf5c8df9a48d0c0348428986f0a9e3873347bccd
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-10-19 01:18:25 -0300

    Add update_reservation and delete_reservation

--- src/graphql/mutations/reservations.jsx
+++ src/graphql/mutations/reservations.jsx
@@ -15,3 +15,25 @@ export const CREATE_RESERVATION = gql`
     }
   }
 `;
+
+export const UPDATE_RESERVATION = gql`
+  mutation UpdateReservationMutation($data: ReservationEditInput!, $idReserva: Int!) {
+    updateReservation(data: $data, id_reserva: $idReserva) {
+      id_reserva
+      finalizada
+      fecha_hora
+      user {
+        cedula
+      }
+      material {
+        id_material
+      }
+    }
+  }
+`;
+
+export const DELETE_RESERVATION = gql`
+  mutation Mutation($idReserva: Int!) {
+    deleteReservation(id_reserva: $idReserva)
+  }
+`;
