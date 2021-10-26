2e651c7d91019daa0555de76219b0182437cbbbd
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-10-26 06:00:57 -0300

    Remove moment

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import React from "react";
-import moment from "moment";
-import es from "moment/locale/es";
 
 import { Router } from "./routes/Router";
-moment.locale("es", es);
 
 const App = () => {
   return <Router />;
