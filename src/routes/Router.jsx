40d29b2cc8335507923667d01b7bbec7645ecf9b
nicoheredia59 <nicoheredia59@gmail.com>
2021-10-27 18:18:39 -0300

    adding user type verification test

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,21 +1,27 @@
-import { BrowserRouter, Switch, Redirect } from "react-router-dom";
-import React, { Suspense, useState } from "react";
-import { Progress } from "@chakra-ui/progress";
-import { useQuery } from "@apollo/client";
-import { ME } from "@graphql/mutations/auth";
+import {BrowserRouter, Switch, Redirect} from "react-router-dom";
+import React, {Suspense, useState} from "react";
+import {Progress} from "@chakra-ui/progress";
+import {useQuery} from "@apollo/client";
+import {ME} from "@graphql/mutations/auth";
 
-import { CartProvider } from "../context/CartProvider";
+import {CartProvider} from "../context/CartProvider";
 
-import { LabRouter } from "./LabRouter";
-import { LandingRouter } from "./LandingRouter";
-import { UserRouter } from "./UserRouter";
+import {LabRouter} from "./LabRouter";
+import {LandingRouter} from "./LandingRouter";
+import {UserRouter} from "./UserRouter";
 
 const PublicRoutes = React.lazy(() => import("./PublicRoutes"));
 const PrivateRoutes = React.lazy(() => import("./PrivateRoutes"));
 
 export const Router = () => {
-  const { data, loading } = useQuery(ME);
+  const {data, loading} = useQuery(ME);
   let isLoggedIn;
+
+  const userType = async () => {
+    const type = await data.me.tipo_usuario;
+    console.log(type);
+  };
+  userType();
 
   if (loading) {
   } else if (!data?.me) {
@@ -24,28 +30,28 @@ export const Router = () => {
     isLoggedIn = true;
   }
 
-  const user = "c";
+  const user = "Laboratorista";
 
   return (
     <BrowserRouter>
       <div>
         <Switch>
-          <Suspense fallback={<Progress isIndeterminate size="xs" />}>
+          <Suspense fallback={<Progress isIndeterminate size='xs' />}>
             <CartProvider>
               <PrivateRoutes
                 component={user == "Laboratorista" ? LabRouter : UserRouter}
                 isAuthenticated={isLoggedIn}
-                path="/app"
+                path='/app'
               />
               <PublicRoutes
                 component={LandingRouter}
                 isAuthenticated={isLoggedIn}
-                path="/"
+                path='/'
                 user={user}
               />
             </CartProvider>
 
-            <Redirect to="/" />
+            <Redirect to='/' />
           </Suspense>
         </Switch>
       </div>
