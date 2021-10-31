f1765dd9a2e7e007df866844a47fc4b1694e23b5
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-10-31 18:48:29 -0300

    Add material search for user

--- src/modules/user/Search/index.jsx
+++ src/modules/user/Search/index.jsx
@@ -5,24 +5,50 @@ import { GET_MATERIAL_SEARCH } from "@graphql/mutations/materials";
 import { useQuery } from "@apollo/client";
 import { Heading } from "@ui";
 import { Image } from "@chakra-ui/image";
+import { Skeleton, SkeletonText } from "@chakra-ui/skeleton";
 
 export const SearchPage = ({ history }) => {
   const { material } = useParams();
+  const skeletonsToRender = [1, 2, 3];
 
-  const { loading, data, error } = useQuery(GET_MATERIAL_SEARCH, {
+  const { data, error } = useQuery(GET_MATERIAL_SEARCH, {
     variables: { nombre: material },
   });
 
-  if (loading && !data) return "loading...";
+  const loading = true;
+
+  if (loading && !data)
+    return (
+      <Stack mt={8} spacing={12}>
+        {skeletonsToRender.map((_, index) => (
+          <Stack key={index} borderRadius="14px" direction="row" p={6} shadow="base">
+            <Skeleton borderRadius="14px" boxSize="120px" />
+
+            <Stack direction="column" spacing={6} w="full">
+              <Skeleton height="20px" w="15%" />
+              <SkeletonText noOfLines={4} spacing="2" />
+            </Stack>
+          </Stack>
+        ))}
+      </Stack>
+    );
 
   return (
     <Box mt={8}>
-      <Stack spacing={8}>
+      <Stack spacing={12}>
         {data.getMaterialSearch.length < 1 ? (
-          <Heading>No resultados</Heading>
+          <Heading>¡La busqueda no arrojo resultados! Intenta con otro material</Heading>
         ) : (
           data.getMaterialSearch.map((material) => (
-            <Stack key={material.id_material} borderRadius="14px" direction="row" p={6} shadow="sm">
+            <Stack
+              key={material.id_material}
+              _hover={{ bg: "lendlab.gray.100" }}
+              borderRadius="14px"
+              cursor="pointer"
+              direction="row"
+              p={6}
+              shadow="base"
+            >
               <Image
                 borderRadius="14px"
                 boxSize="120px"
@@ -30,18 +56,22 @@ export const SearchPage = ({ history }) => {
                 objectFit="cover"
                 src={material.foto}
               />
-              <Stack
-                flex
-                alignItems="start"
-                direction="row"
-                h="auto"
-                justifyContent="space-between"
-                w="full"
-              >
-                <Text fontSize="4" fontWeight="bold" textAlign="left">
-                  {material.nombre}
-                </Text>
-                <Badge boxSize="auto">{material.categoria}</Badge>
+              <Stack w="full">
+                <Stack
+                  flex
+                  alignItems="start"
+                  direction="row"
+                  h="auto"
+                  justifyContent="space-between"
+                  w="full"
+                >
+                  <Text fontSize="4" fontWeight="bold" textAlign="left">
+                    {material.nombre}
+                  </Text>
+                  <Badge boxSize="auto">{material.categoria}</Badge>
+                </Stack>
+
+                <Text>{material.descripcion}</Text>
               </Stack>
             </Stack>
           ))
