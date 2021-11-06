c615f4f8e6d5c5c9e39dca9591ff7c8c121f0598
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-11-06 07:49:28 -0300

    Add Responsive

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,29 +1,39 @@
 import { Box, Container, Heading, Stack } from "@chakra-ui/layout";
-import { Button } from "@chakra-ui/react";
+import { Button, useBreakpointValue } from "@chakra-ui/react";
 import React from "react";
 import { Link } from "react-router-dom";
 
 import ExportButton from "./ExportButton";
 
 const Dashboard = ({ children, hasNoActions, title, link }) => {
+  const isFullWidth = useBreakpointValue({ base: true, md: false });
+
   return (
     <>
-      <Box borderBottom="1px solid" borderBottomColor="lendlab.light.black.300">
+      <Box
+        borderBottom="1px solid"
+        borderBottomColor="lendlab.light.black.300"
+        py={{ base: "12", md: "0" }}
+      >
         <Container variant="normal">
           <Stack
             alignItems="center"
-            direction="row"
-            justifyContent="space-between"
+            direction={{ md: "row", base: "column" }}
+            justifyContent={{ md: "space-between", base: "center" }}
             minH="44"
             w="full"
           >
             <Heading>{title}</Heading>
             {!hasNoActions && (
-              <Stack direction="row" spacing="4">
-                <Button as={Link} to={link} variant="primary">
+              <Stack
+                direction={{ md: "row", base: "column" }}
+                spacing="4"
+                w={{ base: "full", md: "auto" }}
+              >
+                <Button isFu as={Link} isFullWidth={isFullWidth} to={link} variant="primary">
                   Nuevo {title}
                 </Button>
-                <ExportButton tabla={title} />
+                <ExportButton isFullWidth={isFullWidth} tabla={title} />
               </Stack>
             )}
           </Stack>
