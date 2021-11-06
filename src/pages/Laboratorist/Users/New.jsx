744ec83d6a83114975c9a3f2297b3147ef4b37af
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-11-05 22:37:28 -0300

    Include UserFields and validateOnChange to false

--- src/pages/Laboratorist/Users/New.jsx
+++ src/pages/Laboratorist/Users/New.jsx
@@ -6,6 +6,8 @@ import { useRegister } from "@graphql/users/custom-hooks";
 import { Form, Formik } from "formik";
 import React from "react";
 
+import UserFields from "./Fields";
+
 const NewUser = () => {
   const [register, { loading, error }] = useRegister();
 
@@ -28,6 +30,7 @@ const NewUser = () => {
             course_id: "",
           },
         }}
+        validateOnChange={false}
         onSubmit={(values, { resetForm }) => {
           return register({
             variables: { data: values },
@@ -40,68 +43,7 @@ const NewUser = () => {
       >
         {({ dirty }) => (
           <Stack as={Form} spacing={6}>
-            <Field
-              control="input"
-              label="Cedula de Usuario"
-              name="cedula"
-              placeholder="ej. 54548246"
-              type="number"
-            />
-            <Field
-              control="input"
-              label="Nombre"
-              name="nombre"
-              placeholder="ej. Marcos Cianzio"
-              type="text"
-            />
-            <Field
-              control="input"
-              label="Contraseña"
-              name="password"
-              placeholder="ej. ******"
-              type="password"
-            />
-            <Field
-              control="input"
-              label="Dirección"
-              name="direccion"
-              placeholder="ej. Sanchez 294"
-              type="text"
-            />
-            <Field
-              control="input"
-              label="Foto del Usuario"
-              name="foto_usuario"
-              placeholder="pone la fotito"
-              type="text"
-            />
-            <Field
-              control="input"
-              label="telefono"
-              name="telefono"
-              placeholder="ej. 45325140"
-              type="number"
-            />
-            <Field
-              control="input"
-              label="Tipo de Usuario"
-              name="tipo_usuario"
-              placeholder="ej. Alumno"
-              type="text"
-            />
-            <Field
-              control="input"
-              label="Fecha de nacimiento"
-              name="fecha_nacimiento"
-              type="date"
-            />
-            <Field
-              control="input"
-              label="ID Institucion"
-              name="institution.id_institution"
-              type="number"
-            />
-            <Field control="input" label="ID Curso" name="course.course_id" type="number" />
+            <UserFields />
             <Button
               isFullWidth
               disabled={!dirty}
