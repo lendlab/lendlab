ac270a83e1a9c52008cca734b15c06dfc4478f7a
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-11-05 22:32:04 -0300

    Include MaterialsFields

--- src/pages/Laboratorist/Materials/New.jsx
+++ src/pages/Laboratorist/Materials/New.jsx
@@ -1,10 +1,11 @@
 import { Button } from "@chakra-ui/button";
 import { Stack } from "@chakra-ui/layout";
 import Dashboard from "@components/Dashboard";
-import { Field } from "@components/Field";
 import { useCreateMaterial } from "@graphql/materials/custom-hooks";
 import { Form, Formik } from "formik";
 import React from "react";
+
+import MaterialsFields from "./Fields";
 
 const NewMaterial = () => {
   const [createMaterial, { loading, data, error }] = useCreateMaterial();
@@ -24,6 +25,7 @@ const NewMaterial = () => {
             id_institution: "",
           },
         }}
+        validateOnChange={false}
         onSubmit={(values, { resetForm }) => {
           return createMaterial({
             variables: { data: { ...values } },
@@ -36,24 +38,7 @@ const NewMaterial = () => {
       >
         {({ dirty }) => (
           <Stack as={Form} spacing={6}>
-            <Field
-              label="Nombre del Material"
-              name="nombre"
-              placeholder="ej. Ceibalita"
-              type="text"
-            />
-            <Field label="Etiqueta" name="etiqueta" placeholder="ej. 12" type="text" />
-            <Field label="Categoría" name="categoria" placeholder="ej. Laptops" type="text" />
-            <Field label="Descripcion" name="descripcion" placeholder="descripcion" type="text" />
-            <Field label="Cantidad" name="cantidad" placeholder="ej.10" type="number" />
-            <Field label="Foto del Material" name="foto" placeholder="Foto" type="text" />
-            <Field label="Estado del material" name="estado" placeholder="ej. Roto" type="text" />
-            <Field
-              label="Id Institucion"
-              name="institution.id_institution"
-              placeholder="ej. 1"
-              type="number"
-            />
+            <MaterialsFields />
             <Button
               isFullWidth
               disabled={!dirty}
