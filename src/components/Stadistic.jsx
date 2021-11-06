c3b5f7f4b48ff6ca71772803cb276550852c4914
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-11-05 22:20:22 -0300

    Create Stadistic

---
+++ src/components/Stadistic.jsx
@@ -0,0 +1,30 @@
+import Icon from "@chakra-ui/icon";
+import { Box, Circle, Stack, Text, WrapItem } from "@chakra-ui/layout";
+import React from "react";
+
+const Stadistic = ({ title, number, icon }) => {
+  return (
+    <WrapItem flex="1" h="full" maxW="72" minH="44">
+      <Box
+        bg="white"
+        borderColor="lendlab.light.black.300"
+        borderWidth="1px "
+        p="6"
+        rounded="md"
+        w="full"
+      >
+        <Stack align="center" direction="row">
+          <Icon as={icon} color="lendlab.light.black.1000" />
+          <Text color="lendlab.light.black.1000" fontWeight="bold">
+            {title}
+          </Text>
+        </Stack>
+        <Text color="lendlab.light.black.1000" fontSize="8" fontWeight="bold" py="2">
+          {number}
+        </Text>
+      </Box>
+    </WrapItem>
+  );
+};
+
+export default Stadistic;
