03d4652e35ba4c6c26cdb3a96eef785a824f6c8f
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-11-05 22:39:38 -0300

    Handle diferent type of user's redirect

--- src/routes/PublicRoutes.jsx
+++ src/routes/PublicRoutes.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { Redirect, Route } from "react-router-dom";
 
-const PublicRoutes = ({ isAuthenticated, user, component: Component, ...rest }) => {
+const PublicRoutes = ({ isAuthenticated, to, component: Component, ...rest }) => {
   return (
     <Route
       {...rest}
-      component={(props) =>
-        isAuthenticated ? <Redirect to="/dashboard/prestamos" /> : <Component {...props} />
-      }
+      component={(props) => (isAuthenticated ? <Redirect to={to} /> : <Component {...props} />)}
     />
   );
 };
