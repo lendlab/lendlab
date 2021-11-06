4882e085443d132b5e148c262db2bb7456cc55b8
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-11-06 07:50:53 -0300

    Add responsive

--- src/components/UserMenu.jsx
+++ src/components/UserMenu.jsx
@@ -1,4 +1,12 @@
-import { Avatar, Icon, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import {
+  Avatar,
+  Icon,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  useBreakpointValue,
+} from "@chakra-ui/react";
 import { useLogout } from "@graphql/auth/custom-hook";
 import React from "react";
 import { FiLogOut } from "react-icons/fi";
@@ -6,15 +14,17 @@ import { FiLogOut } from "react-icons/fi";
 const UserMenu = ({ name }) => {
   const [logout, { loading }, apolloClient] = useLogout();
 
+  const size = useBreakpointValue({ base: "sm", md: "md" });
+
   return (
     <Menu isLazy placement="bottom-end">
       <MenuButton>
-        <Avatar alt={name} name={name} />
+        <Avatar alt={name} name={name} size={size} />
       </MenuButton>
 
       <MenuList>
         <MenuItem
-          icon={<Icon as={FiLogOut} isLoading={loading} />}
+          icon={<Icon as={FiLogOut} />}
           onClick={async () => {
             await logout();
             await apolloClient.resetStore();
