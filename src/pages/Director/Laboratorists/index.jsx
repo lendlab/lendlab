e6902c31689deb608f53b7f09d618ca54d4267b7
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-11-05 22:29:07 -0300

    Add Laboratorists

---
+++ src/pages/Director/Laboratorists/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Dashboard from "@components/Dashboard";
+
+const Laboratorists = () => {
+  return <Dashboard link="/director/laboratoristas/nuevo" title="Laboratoristas" />;
+};
+
+export default Laboratorists;
