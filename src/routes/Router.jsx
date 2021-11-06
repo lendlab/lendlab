a87fb31dac0695c4007cdc48e39dde98b852417d
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-11-05 22:40:06 -0300

    Add login and different type of user's router handling

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,33 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Redirect, Switch } from "react-router-dom";
+import { useMe } from "@graphql/auth/custom-hook";
+import { usersTypes } from "@utils/constants/usersTypes";
+import { types_router } from "@utils/constants/types_routers";
 
-import DashboardRoutes from "./DashboardRouter";
 import PrivateRoutes from "./PrivateRoutes";
 import PublicRoutes from "./PublicRoutes";
-import StudentRouter from "./StudentRouter";
 import UnloggedRouter from "./UnloggedRouter";
 
 export const Router = () => {
-  const isLoggedIn = true;
-  const user = "Laboratorista";
+  let isLoggedIn = null;
+  let path = "/aasdasd";
+  let component = null;
+
+  const { loading, data } = useMe();
+
+  if (loading) {
+    return null;
+  } else if (data?.me) {
+    isLoggedIn = true;
+    path = usersTypes[data?.me.tipo_usuario];
+    component = types_router[data?.me.tipo_usuario];
+  } else {
+    isLoggedIn = false;
+  }
 
   return (
     <BrowserRouter>
       <>
         <Switch>
-          <PrivateRoutes
-            component={user == "Laboratorista" ? DashboardRoutes : StudentRouter}
-            isAuthenticated={isLoggedIn}
-            path={user == "Laboratorista" ? "/dashboard" : "/app"}
-          />
+          <PrivateRoutes component={component} isAuthenticated={isLoggedIn} path={path} />
           <PublicRoutes
             component={UnloggedRouter}
             isAuthenticated={isLoggedIn}
             path="/"
-            user={user}
+            to={path}
           />
-
-          <Redirect to="/" />
         </Switch>
       </>
     </BrowserRouter>
