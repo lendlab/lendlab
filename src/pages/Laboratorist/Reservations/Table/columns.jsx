e5886cc832d09f5f8c1f2dd9d73e365edf827299
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-11-05 22:34:28 -0300

    Add deleteReservation and editReservation buttons

--- src/pages/Laboratorist/Reservations/Table/columns.jsx
+++ src/pages/Laboratorist/Reservations/Table/columns.jsx
@@ -1,9 +1,13 @@
 import { Avatar } from "@chakra-ui/avatar";
+import { IconButton } from "@chakra-ui/button";
+import Icon from "@chakra-ui/icon";
 import { Badge, Stack } from "@chakra-ui/layout";
 import { chakra } from "@chakra-ui/system";
 import { Tooltip } from "@chakra-ui/tooltip";
+import { useDeleteReservation } from "@graphql/reservations/custom-hooks";
 import { momentizeDate } from "@utils/momentizeDate";
 import React from "react";
+import { FiEdit2, FiTrash } from "react-icons/fi";
 
 export const COLUMNS = [
   {
@@ -36,7 +40,7 @@ export const COLUMNS = [
     },
     Cell({ row }) {
       return (
-        <Badge colorScheme={row.original.finalizada ? "green" : "red"}>
+        <Badge colorScheme={row.original.finalizada ? "green" : "red"} variant="solid">
           {row.original.finalizada ? "SI" : "NO"}
         </Badge>
       );
@@ -73,6 +77,42 @@ export const COLUMNS = [
       });
 
       return output.join(", ");
+    },
+  },
+  {
+    header: "",
+    id: "click-me-button",
+    Cell({ row }) {
+      const [deleteReservation, { loading, data }] = useDeleteReservation();
+
+      return (
+        <Stack direction="row">
+          <IconButton
+            aria-label="Borrar Material"
+            icon={<Icon as={FiTrash} color="lendlab.light.red.400" />}
+            isLoading={loading}
+            variant="ghost"
+            onClick={() => {
+              deleteReservation({
+                variables: {
+                  idReserva: parseInt(row.original.id_reserva),
+                },
+                update: (cache) => {
+                  cache.evict({
+                    id_reserva: "Reservation:" + row.original.id_reserva,
+                  });
+                },
+              });
+            }}
+          />
+          <IconButton
+            aria-label="Editar Material"
+            color="lendlab.light.red.400"
+            icon={<Icon as={FiEdit2} color="lendlab.yellow" />}
+            variant="ghost"
+          />
+        </Stack>
+      );
     },
   },
 ];
