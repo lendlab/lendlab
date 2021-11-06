eb90c0e1e79fc7b87226f1f16d9f13e28301f093
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-11-05 22:15:56 -0300

    Add WebSocketLink to include the new suscriptions

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,43 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { ChakraProvider, CSSReset, ColorModeScript } from "@chakra-ui/react";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
-
+import { ApolloClient, ApolloProvider, HttpLink, InMemoryCache, split } from "@apollo/client";
+import { WebSocketLink } from "@apollo/client/link/ws";
+import { getMainDefinition } from "@apollo/client/utilities";
 import "@babel/polyfill";
+import { ChakraProvider, ColorModeScript, CSSReset } from "@chakra-ui/react";
+import "@fontsource/archivo";
+import "@fontsource/manrope";
+import React from "react";
+import "react-datepicker/dist/react-datepicker.css";
+import ReactDOM from "react-dom";
 
 import App from "./App";
-import "@fontsource/manrope";
-import "@fontsource/archivo";
-import theme from "./styles/theme";
-import "react-datepicker/dist/react-datepicker.css";
+import { GET_INSTITUTIONS } from "./graphql/institutions/graphql-queries";
 import "./styles/date-time-picker.css";
+import theme from "./styles/theme";
+
+const httpLink = new HttpLink({
+  uri: "https://lendlab-backend-9nbzz.ondigitalocean.app/api",
+  credentials: "include",
+});
+
+const wsLink = new WebSocketLink({
+  uri: "wss://lendlab-backend-9nbzz.ondigitalocean.app/api",
+  options: {
+    reconnect: true,
+  },
+});
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+
+    return definition.kind === "OperationDefinition" && definition.operation === "subscription";
+  },
+  wsLink,
+  httpLink
+);
 
 const client = new ApolloClient({
-  uri: "https://lendlab-backend-9nbzz.ondigitalocean.app/api",
+  link: splitLink,
   cache: new InMemoryCache(),
   credentials: "include",
 });
