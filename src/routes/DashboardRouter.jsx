c30cf0987e86addfa22503a90e3c1cc9141891fd
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-11-05 22:39:07 -0300

    Add some new routes

--- src/routes/DashboardRouter.jsx
+++ src/routes/DashboardRouter.jsx
@@ -1,32 +1,39 @@
+import Footer from "@components/Footer";
+import TabsNav from "@components/Tabs/TabsNav";
+import Incidents from "@pages/Laboratorist/Incidents";
+import Lends from "@pages/Laboratorist/Lends";
+import Lend from "@pages/Laboratorist/Lends/Lend";
+import NewLend from "@pages/Laboratorist/Lends/New";
+import Materials from "@pages/Laboratorist/Materials";
+import Material from "@pages/Laboratorist/Materials/Material";
+import NewMaterial from "@pages/Laboratorist/Materials/New";
+import Reservations from "@pages/Laboratorist/Reservations";
+import Summary from "@pages/Laboratorist/Summary";
+import Users from "@pages/Laboratorist/Users";
+import NewUser from "@pages/Laboratorist/Users/New";
+import { DASHBOARD_ROUTES } from "@utils/constants/routes";
 import React from "react";
 import { Redirect, Route, Switch } from "react-router";
-import Lends from "@pages/Laboratorist/Lends";
-import Materials from "@pages/Laboratorist/Materials";
-import NewLend from "@pages/Laboratorist/Lends/New";
-import Users from "@pages/Laboratorist/Users";
-import DashboardTabs from "@components/DashboardTabs";
-import Reservations from "@pages/Laboratorist/Reservations";
-import Footer from "@components/Footer";
-import NewMaterial from "@pages/Laboratorist/Materials/New";
-import NewUser from "@pages/Laboratorist/Users/New";
 
 const DashboardRoutes = () => {
   return (
     <>
-      <DashboardTabs />
+      <TabsNav routes={DASHBOARD_ROUTES} />
       <Switch>
-        {/* <Route component={Summary} path="/dashboard/resumen" /> */}
+        <Route exact component={Summary} path="/dashboard/resumen" />
 
         <Route exact component={Lends} path="/dashboard/prestamos" />
         <Route exact component={NewLend} path="/dashboard/prestamos/nuevo" />
-        {/* <Route component={Lend} path="/dashboard/prestamo/:id" /> */}
+        <Route exact component={Lend} path="/dashboard/prestamos/:id" />
+
+        <Route exact component={Incidents} path="/dashboard/incidentes" />
 
         <Route exact component={Reservations} path="/dashboard/reservas" />
         {/* <Route component={LabReservesPage} path="/dashboard/reserva/:id" /> */}
 
         <Route exact component={Materials} path="/dashboard/materiales" />
         <Route exact component={NewMaterial} path="/dashboard/materiales/nuevo" />
-        {/* <Route component={LabMaterialsPage} path="/dashboard/material/:id" /> */}
+        <Route exact component={Material} path="/dashboard/materiales/:id" />
 
         <Route exact component={Users} path="/dashboard/usuarios" />
         <Route exact component={NewUser} path="/dashboard/usuarios/nuevo" />
