fd45dfdf972520e1a3faf525e8d71b4b97e65162
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-11-06 07:48:15 -0300

    Add /

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "/src"),
+      "@": path.resolve(__dirname, "src"),
       "@ui": path.resolve(__dirname, "src/ui"),
       "@pages": path.resolve(__dirname, "src/pages"),
       "@utils": path.resolve(__dirname, "src/utils"),
