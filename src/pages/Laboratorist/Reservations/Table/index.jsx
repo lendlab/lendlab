d62605d28ef71be6ffe88ba0c7cc8e3375f9789a
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-11-05 22:34:49 -0300

    Add subscriptions to the table

--- src/pages/Laboratorist/Reservations/Table/index.jsx
+++ src/pages/Laboratorist/Reservations/Table/index.jsx
@@ -1,13 +1,14 @@
 import { Table } from "@components/Table";
 import TableSkeleton from "@components/Table/TableSkeleton";
 import { useReservationsAndMaxId } from "@graphql/reservations/custom-hooks";
+import { RESERVATIONS_SUSCRIPTION } from "@graphql/reservations/graphql-suscriptions";
 import { groupAndMerge } from "@utils/groupAndMerge";
 import React from "react";
 
 import { COLUMNS } from "./columns";
 
 const ReservationsTable = () => {
-  const { loading, error, data } = useReservationsAndMaxId();
+  const [{ subscribeToMore, loading, error, data }, document] = useReservationsAndMaxId();
   let mergedReservations;
 
   if (loading || !data) {
@@ -20,7 +21,31 @@ const ReservationsTable = () => {
     return error.message;
   }
 
-  return <Table columns={COLUMNS} data={mergedReservations} />;
+  return (
+    <Table
+      columns={COLUMNS}
+      data={mergedReservations}
+      id="Reservas"
+      subscribeToNew={() => {
+        subscribeToMore({
+          document: RESERVATIONS_SUSCRIPTION,
+
+          updateQuery: (prev, { subscriptionData }) => {
+            console.log({ prev, subscriptionData });
+            if (!subscriptionData.data) return prev;
+
+            const newSuscription = subscriptionData.data.newReservationSubscription;
+
+            console.log({ newSuscription });
+
+            return Object.assign({}, prev, {
+              getReservations: [newSuscription, ...prev.getReservations],
+            });
+          },
+        });
+      }}
+    />
+  );
 };
 
 export default ReservationsTable;
