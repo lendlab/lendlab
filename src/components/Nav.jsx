6207f56b21da185d95620a5ae4b9d5479bd47b1f
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-11-06 07:50:26 -0300

    Render the nav base in the type of user

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,20 +4,28 @@ import React from "react";
 import { useMe } from "@graphql/auth/custom-hook";
 import { Avatar } from "@chakra-ui/avatar";
 import { Link } from "react-router-dom";
+import { Input } from "@chakra-ui/input";
+import { Form, Formik } from "formik";
+import { FiSearch } from "react-icons/fi";
+import { useHistory } from "react-router-dom";
 
 import UserMenu from "./UserMenu";
+import { Field } from "./Field";
 
-const Nav = ({ children }) => {
+const Nav = ({ children, studentNav }) => {
   const { loading, data, error } = useMe();
 
-  let options = null;
+  const history = useHistory();
 
-  {
-    /* <Avatar alt={data?.me.nombre} name={data?.me.nombre} size="md" /> */
-  }
+  let options = null;
+  let isStudent = false;
+
   if (loading) {
   } else if (data?.me) {
     options = <UserMenu name={data?.me.nombre} />;
+    if (data?.me.tipo_usuario == "Alumno") {
+      isStudent = true;
+    }
   } else {
     options = (
       <>
@@ -31,25 +39,69 @@ const Nav = ({ children }) => {
   }
 
   return (
-    <>
+    <Stack>
       <Container variant="normal">
         <Stack
-          alignItems="center"
+          alignItems={isStudent ? { md: "center", base: "start" } : "center"}
           as="nav"
-          direction="row"
-          h="36px"
+          direction={isStudent && { md: "row", base: "column" }}
+          height={isStudent ? { md: "36px", base: "auto" } : "36px"}
           justifyContent="space-between"
+          minH="36px"
           py="8"
+          spacing={{ md: "8", base: "2" }}
           zIndex="100"
         >
-          <Heading>LendLab</Heading>
-          <Stack alignItems="center" direction="row" spacing="8">
+          <Stack
+            alignItems="center"
+            direction="row"
+            justifyContent="space-between"
+            w={{ md: "auto", base: "full" }}
+          >
+            <Heading>LendLab</Heading>
+            {isStudent && (
+              <Stack
+                alignItems="center"
+                direction="row"
+                display={{ md: "none", base: "flex" }}
+                spacing="8"
+              >
+                {options}
+              </Stack>
+            )}
+          </Stack>
+          {isStudent && (
+            <Formik
+              initialValues={{ material: "" }}
+              validateOnChange={false}
+              onSubmit={() => {
+                history.push(`/app/busqueda/${material}`);
+              }}
+            >
+              {() => (
+                <Stack as={Form} spacing="0" w="full">
+                  <Field
+                    isFullWidth
+                    icon={FiSearch}
+                    name="material"
+                    placeholder="Buscar material..."
+                  />
+                </Stack>
+              )}
+            </Formik>
+          )}
+          <Stack
+            alignItems="center"
+            direction="row"
+            display={isStudent && { md: "flex", base: "none" }}
+            spacing="8"
+          >
             {options}
           </Stack>
         </Stack>
       </Container>
       {children}
-    </>
+    </Stack>
   );
 };
 
