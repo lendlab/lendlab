82cd616e7c6b1e0c8490ddcf7faa0e561d9cae2a
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-11-05 22:24:21 -0300

    Add useDeleteMaterial and useUpdateMaterial

--- src/graphql/materials/custom-hooks.jsx
+++ src/graphql/materials/custom-hooks.jsx
@@ -1,8 +1,8 @@
 import { useMutation, useQuery } from "@apollo/client";
 import { useToast } from "@chakra-ui/react";
 
-import { CREATE_MATERIAL } from "./graphql-mutations";
-import { GET_ALL_MATERIALS } from "./graphql-queries";
+import { CREATE_MATERIAL, DELETE_MATERIAL, UPDATE_MATERIAL } from "./graphql-mutations";
+import { GET_ALL_MATERIALS, GET_MATERIAL } from "./graphql-queries";
 
 export const useMaterials = () => {
   const result = useQuery(GET_ALL_MATERIALS);
@@ -25,4 +25,45 @@ export const useCreateMaterial = () => {
   });
 
   return [createMaterial, result];
+};
+
+export const useDeleteMaterial = () => {
+  const toast = useToast();
+
+  const [deleteMaterial, result] = useMutation(DELETE_MATERIAL, {
+    onCompleted: () =>
+      toast({
+        title: "Material eliminado con exito",
+        description: "Se ha eliminado correctamente el material",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+      }),
+  });
+
+  return [deleteMaterial, result];
+};
+
+export const useMaterial = (id) => {
+  const result = useQuery(GET_MATERIAL, { variables: { idMaterial: parseInt(id) } });
+
+  return result;
+};
+
+export const useUpdateMaterial = () => {
+  const toast = useToast();
+
+  const [updateMaterial] = useMutation(UPDATE_MATERIAL, {
+    onCompleted: ({ updateMaterial }) => {
+      toast({
+        title: "Material modificado con éxito",
+        description: "Se ha modificado correctamente el material " + updateMaterial.nombre,
+        status: "success",
+        duration: 2000,
+        isClosable: true,
+      });
+    },
+  });
+
+  return updateMaterial;
 };
