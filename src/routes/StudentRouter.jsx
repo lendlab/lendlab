5e6ca4a91b4a96f1bbe95c6a8714fb31df92e462
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-11-07 19:26:00 -0300

    Add CartModal

--- src/routes/StudentRouter.jsx
+++ src/routes/StudentRouter.jsx
@@ -1,21 +1,29 @@
+import { Stack } from "@chakra-ui/layout";
+import CartModal from "@components/CartModal";
 import Footer from "@components/Footer";
 import TabsNav from "@components/Tabs/TabsNav";
+import Config from "@pages/Config";
 import Home from "@pages/Student/Home";
+import Search from "@pages/Student/Search";
 import { STUDENT_ROUTES } from "@utils/constants/routes";
 import React from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
 
 const StudentRouter = () => {
   return (
-    <>
+    <Stack h="100%" minH="100vh" spacing="0">
       <TabsNav routes={STUDENT_ROUTES} />
       <Switch>
         <Route exact component={Home} path="/app/inicio" />
+        <Route exact component={Search} path="/app/busqueda/:material" />
+        <Route exact component={Config} path="/app/configuracion" />
         <Redirect to="/app/inicio" />
       </Switch>
-
+      <Stack align="center" bottom="0" p="4" position="fixed" w="full">
+        <CartModal />
+      </Stack>
       <Footer />
-    </>
+    </Stack>
   );
 };
 
