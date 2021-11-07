31a4afc97eed3b87c7b50d2d17e4fb946cba6d68
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-11-07 19:13:25 -0300

    Add reservation and laboratorist, and updateLend query

--- src/graphql/lends/graphql-mutations.jsx
+++ src/graphql/lends/graphql-mutations.jsx
@@ -7,11 +7,31 @@ export const CREATE_LEND = gql`
       fecha_hora_presta
       fecha_vencimiento
       fecha_devolucion
-      laboratorist
       reservation {
         id_reserva
         fecha_hora
       }
+      laboratorist {
+        cedula
+      }
+    }
+  }
+`;
+
+export const UPDATE_LEND = gql`
+  mutation Mutation($data: LendUpdateInput!, $idLend: Int!) {
+    updateLend(data: $data, id_lend: $idLend) {
+      id_lend
+      fecha_devolucion
+      reservation {
+        id_reserva
+        fecha_hora
+      }
+      laboratorist {
+        cedula
+      }
+      fecha_vencimiento
+      fecha_hora_presta
     }
   }
 `;
