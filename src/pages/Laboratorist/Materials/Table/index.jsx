3d705e729bcad79a1e83ddb1641204023521a416
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-11-07 19:20:38 -0300

    Add subscribeToMore

--- src/pages/Laboratorist/Materials/Table/index.jsx
+++ src/pages/Laboratorist/Materials/Table/index.jsx
@@ -2,11 +2,12 @@ import { Table } from "@components/Table";
 import React from "react";
 import { useMaterials } from "@graphql/materials/custom-hooks";
 import TableSkeleton from "@components/Table/TableSkeleton";
+import { SUBSCRIBE_TO_MATERIALS } from "@graphql/materials/graphql-subscriptions";
 
 import { COLUMNS } from "./columns";
 
 const MaterialsTable = () => {
-  const { loading, error, data } = useMaterials();
+  const { loading, error, data, subscribeToMore } = useMaterials();
 
   if (loading || !data) {
     return (
@@ -29,7 +30,28 @@ const MaterialsTable = () => {
     return error.message;
   }
 
-  return <Table columns={COLUMNS} data={data.getMaterials} id="Materiales" />;
+  return (
+    <Table
+      columns={COLUMNS}
+      data={data.getMaterials}
+      id="Materiales"
+      subscribeToNew={() => {
+        subscribeToMore({
+          document: SUBSCRIBE_TO_MATERIALS,
+
+          updateQuery: (prev, { subscriptionData }) => {
+            if (!subscriptionData.data) return prev;
+
+            const newSuscription = subscriptionData.data.newMaterialSubscription;
+
+            return Object.assign({}, prev, {
+              getMaterials: [newSuscription, ...prev.getMaterials],
+            });
+          },
+        });
+      }}
+    />
+  );
 };
 
 export default MaterialsTable;
--- src/pages/Laboratorist/Users/Table/index.jsx
+++ src/pages/Laboratorist/Users/Table/index.jsx
@@ -2,11 +2,12 @@ import { Table } from "@components/Table";
 import TableSkeleton from "@components/Table/TableSkeleton";
 import { useUsers } from "@graphql/users/custom-hooks";
 import React from "react";
+import { SUBSCRIBE_TO_USERS } from "@graphql/users/graphql-subscriptions";
 
 import { COLUMNS } from "./columns";
 
 const UsersTable = () => {
-  const { loading, error, data } = useUsers();
+  const { loading, error, data, subscribeToMore } = useUsers();
 
   if (loading || !data) {
     return (
@@ -28,7 +29,28 @@ const UsersTable = () => {
     return error.message;
   }
 
-  return <Table columns={COLUMNS} data={data.getUsers} id="Usuarios" />;
+  return (
+    <Table
+      columns={COLUMNS}
+      data={data.getUsers}
+      id="Usuarios"
+      subscribeToNew={() => {
+        subscribeToMore({
+          document: SUBSCRIBE_TO_USERS,
+
+          updateQuery: (prev, { subscriptionData }) => {
+            if (!subscriptionData.data) return prev;
+
+            const newSuscription = subscriptionData.data.newUserSubscription;
+
+            return Object.assign({}, prev, {
+              getUsers: [newSuscription, ...prev.getUsers],
+            });
+          },
+        });
+      }}
+    />
+  );
 };
 
 export default UsersTable;
