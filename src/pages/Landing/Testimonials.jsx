3795d08c25ae6892f43edf2d57f3704a173c40d4
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-11-07 19:24:39 -0300

    Translate testimonials

--- src/pages/Landing/Testimonials.jsx
+++ src/pages/Landing/Testimonials.jsx
@@ -14,34 +14,34 @@ import React from "react";
 
 const testimonials = [
   {
-    name: "Brandon P.",
+    name: "Gianni Rosas",
     role: "Chief Marketing Officer",
     content:
-      "It really saves me time and effort. It is exactly what our business has been lacking. EEZY is the most valuable business resource we have EVER purchased. After using EEZY my business skyrocketed!",
+      "Realmente me ahorra tiempo y esfuerzo. Es exactamente lo que le ha faltado a nuestro trabajo. LendLab es el recurso más valioso que NUNCA hemos consumido. Realmente lo recomiendo.",
     avatar:
       "https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
   },
   {
-    name: "Krysta B.",
+    name: "Mercedes Khars",
     role: "Entrepreneur",
     content:
-      "I didn't even need training. We've used EEZY for the last five years. I have gotten at least 50 times the value from EEZY. I made back the purchase price in just 48 hours!",
+      "¡Buen trabajo, definitivamente volveré a utilizarlo! LendLab superó por completo nuestras expectativas. Es excelente y sorprendentemente todo es simplemente gratuito",
     avatar:
       "https://images.unsplash.com/photo-1598550874175-4d0ef436c909?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
   },
   {
-    name: "Darcy L.",
-    role: "Movie star",
+    name: "Josefina ",
+    role: "Laboratorista",
     content:
-      "Thank you for making it painless, pleasant and most of all, hassle free! I'm good to go. No matter where you go, EEZY is the coolest, most happening thing around! I love EEZY!",
+      "Hombre, esto está mejorando cada vez más a medida que aprendo más sobre el producto. Está todo bien. ¡Buen trabajo, definitivamente volveré a a usarlo! Gracias por el gran servicio.",
     avatar:
       "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=334&q=80",
   },
   {
-    name: "Daniel T.",
-    role: "Musician",
+    name: "Rodrigo Villar",
+    role: "Alumno",
     content:
-      "I am so pleased with this product. EEZY is both attractive and highly adaptable. Without EEZY, we would have gone bankrupt by now. Thank you for creating this product!",
+      "Amo el sistema. También me gustaría agradecer a todo su personal. ¡Lo mejor de la red! Si desea un marketing real que funcione y una implementación efectiva, LendLab lo tiene cubierto",
     avatar:
       "https://images.unsplash.com/photo-1606513542745-97629752a13b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
   },
