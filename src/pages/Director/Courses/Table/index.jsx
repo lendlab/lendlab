17ffb50fd2dbef1bd46d8ae20818f419a73d7c75
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-11-09 11:17:42 -0300

    Add CoursesTable

---
+++ src/pages/Director/Courses/Table/index.jsx
@@ -0,0 +1,40 @@
+import { Table } from "@components/Table";
+import TableSkeleton from "@components/Table/TableSkeleton";
+import { useMe } from "@graphql/auth/custom-hook";
+import { SUBSCRIBE_TO_USERS } from "@graphql/users/graphql-subscriptions";
+import React from "react";
+
+import { COLUMNS } from "./columns";
+import { useCoursesByInstitution } from "@graphql/courses/custom-hooks";
+
+const CoursesTable = () => {
+  const { data: dataMe } = useMe();
+
+  const { loading, error, data, subscribeToMore } = useCoursesByInstitution(dataMe?.me.course.institution.id_institution);
+
+  if (loading || !data) {
+    return (
+      <TableSkeleton
+        theads={[
+          "Token",
+          "Nombre",
+          "Institución",
+        ]}
+      />
+    );
+  }
+
+  if (error) {
+    return error.message;
+  }
+
+  return (
+    <Table
+      columns={COLUMNS}
+      data={data.getCoursessByInstitution}
+      id="Cursos"
+    />
+  );
+};
+
+export default CoursesTable;
