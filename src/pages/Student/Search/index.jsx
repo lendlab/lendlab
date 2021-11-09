554f6efd4936a1394f068a5f4655b622175a3a4c
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-11-09 06:16:00 -0300

    Handle when there is no data

--- src/pages/Student/Search/index.jsx
+++ src/pages/Student/Search/index.jsx
@@ -1,7 +1,10 @@
+import Icon from "@chakra-ui/icon";
+import { Stack, Text } from "@chakra-ui/layout";
 import Dashboard from "@components/Dashboard";
 import MaterialSearchItem from "@components/MaterialSearchItem";
 import { useMaterialSearch } from "@graphql/materials/custom-hooks";
 import React from "react";
+import { FiFrown } from "react-icons/fi";
 import { useParams } from "react-router";
 
 const Search = () => {
@@ -13,9 +16,27 @@ const Search = () => {
 
   return (
     <Dashboard hasNoActions title={material}>
-      {data.getMaterialSearch.map((material) => (
-        <MaterialSearchItem key={material.id_material} material={material} />
-      ))}
+      <Stack spacing="6">
+        {data.getMaterialSearch.length > 0 ? (
+          data.getMaterialSearch.map((material) => (
+            <MaterialSearchItem key={material.id_material} material={material} />
+          ))
+        ) : (
+          <Stack
+            alignItems="center"
+            bg="white"
+            border="1px solid"
+            borderColor="lendlab.light.black.300"
+            direction="row"
+            justifyContent="center"
+            py={8}
+            rounded="md"
+          >
+            <Icon as={FiFrown} color="lendlab.light.black.800" />
+            <Text>No se encontraron resultados</Text>
+          </Stack>
+        )}
+      </Stack>
     </Dashboard>
   );
 };
