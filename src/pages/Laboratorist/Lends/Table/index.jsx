c0ff83ceceddcd867ebf5d78b0c87382f8794fc5
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-11-09 06:12:02 -0300

    Add subscribeToNew

--- src/pages/Laboratorist/Lends/Table/index.jsx
+++ src/pages/Laboratorist/Lends/Table/index.jsx
@@ -1,12 +1,18 @@
 import { Table } from "@components/Table";
-import { useLends } from "@graphql/lends/custom-hooks";
-import React from "react";
 import TableSkeleton from "@components/Table/TableSkeleton";
+import { useMe } from "@graphql/auth/custom-hook";
+import { useLendsByInstitution } from "@graphql/lends/custom-hooks";
+import { SUBSCRIBE_TO_LENDS } from "@graphql/lends/graphql-subscriptions";
+import React from "react";
 
 import { COLUMNS } from "./columns";
 
 const LendsTable = () => {
-  const { loading, error, data } = useLends();
+  const { data: dataMe } = useMe();
+
+  const { loading, error, data, subscribeToMore } = useLendsByInstitution(
+    dataMe?.me.course.institution.id_institution
+  );
 
   if (loading || !data) {
     return <TableSkeleton theads={["ID", "Fecha y Hora", "Plazo", "Devolucion", "Reserva"]} />;
@@ -16,7 +22,28 @@ const LendsTable = () => {
     return error.message;
   }
 
-  return <Table columns={COLUMNS} data={data.lend} id="Prestamos" />;
+  return (
+    <Table
+      columns={COLUMNS}
+      data={data.getLendsByInstitution}
+      id="Prestamos"
+      subscribeToNew={() => {
+        subscribeToMore({
+          document: SUBSCRIBE_TO_LENDS,
+
+          updateQuery: (prev, { subscriptionData }) => {
+            if (!subscriptionData.data) return prev;
+
+            const newSuscription = subscriptionData.data.newLendSubscription;
+
+            return Object.assign({}, prev, {
+              getLendsByInstitution: [newSuscription, ...prev.getLendsByInstitution],
+            });
+          },
+        });
+      }}
+    />
+  );
 };
 
 export default LendsTable;
