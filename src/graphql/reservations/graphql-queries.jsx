d73381143ec6648da35460ac84781bcae506017c
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-11-09 06:08:54 -0300

    Add get_reservations_by_institution

--- src/graphql/reservations/graphql-queries.jsx
+++ src/graphql/reservations/graphql-queries.jsx
@@ -42,3 +42,69 @@ export const GET_RESERVATION_MAX_ID = gql`
     getMaxId
   }
 `;
+
+export const GET_RESERVATIONS_BY_INSTITUTION = gql`
+  query GetReservationsByInstitution($idInstitution: Int!) {
+    getReservationsByInstitution(id_institution: $idInstitution) {
+      id_reserva
+      fecha_hora
+      finalizada
+      user {
+        cedula
+        nombre
+        direccion
+        foto_usuario
+        telefono
+        tipo_usuario
+        fecha_nacimiento
+        course {
+          course_name
+          course_token
+          institution {
+            id_institution
+            institution_name
+          }
+        }
+      }
+      material {
+        id_material
+        nombre
+        etiqueta
+        categoria
+        descripcion
+        cantidad
+        estado
+      }
+      institution {
+        id_institution
+        institution_name
+        city
+        type
+        phone
+      }
+    }
+    getMaxId
+  }
+`;
+
+export const GET_USER_RESERVATIONS = gql`
+  query Query($cedula: Int!) {
+    getUserReservations(cedula: $cedula) {
+      id_reserva
+      fecha_hora
+      finalizada
+      user {
+        cedula
+        nombre
+      }
+      material {
+        id_material
+        nombre
+      }
+      institution {
+        id_institution
+        institution_name
+      }
+    }
+  }
+`;
