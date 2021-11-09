798c3e29d74ded403c71c623fca64f536a02e01d
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-11-09 11:17:56 -0300

    Add LaboratoristsTable

--- src/pages/Director/Laboratorists/index.jsx
+++ src/pages/Director/Laboratorists/index.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import Dashboard from "@components/Dashboard";
+import LaboratoristsTable from "./Table";
 
 const Laboratorists = () => {
-  return <Dashboard link="/director/laboratoristas/nuevo" title="Laboratoristas" />;
+  return <Dashboard link="/director/laboratoristas/nuevo" title="Laboratoristas" >
+    <LaboratoristsTable />
+  </Dashboard>;
 };
 
 export default Laboratorists;
