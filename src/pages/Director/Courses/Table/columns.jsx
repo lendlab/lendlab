b52a2cc40e49133a97535a2ce94c1e928d19b5e2
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-11-09 11:17:25 -0300

    Add columns

---
+++ src/pages/Director/Courses/Table/columns.jsx
@@ -0,0 +1,62 @@
+import { IconButton } from "@chakra-ui/button";
+import Icon from "@chakra-ui/icon";
+import { Badge, Stack } from "@chakra-ui/layout";
+import { useDeleteUser } from "@graphql/users/custom-hooks";
+import { momentizeDate } from "@utils/momentizeDate";
+import React from "react";
+import { FiEdit2, FiTrash } from "react-icons/fi";
+import { useHistory } from "react-router";
+
+export const COLUMNS = [
+  {
+    Header: "Token",
+    accessor: "course_token",
+  },
+  {
+    Header: "Nombre",
+    accessor: "course_name",
+  },
+  {
+    Header: "Institución",
+    accessor: "institution.institution_name",
+  },
+  //   {
+  //     header: "",
+  //     id: "click-me-button",
+  //     Cell({ row }) {
+  //       const [deleteUser, { loading, data }] = useDeleteUser();
+
+  //       const history = useHistory();
+
+  //       return (
+  //         <Stack direction="row">
+  //           <IconButton
+  //             aria-label="Borrar Material"
+  //             icon={<Icon as={FiTrash} color="lendlab.light.red.400" />}
+  //             isLoading={loading}
+  //             variant="ghost"
+  //             onClick={() => {
+  //               deleteUser({
+  //                 variables: {
+  //                   cedula: row.original.cedula,
+  //                 },
+  //                 update: (cache) => {
+  //                   cache.evict({
+  //                     id_material: "User:" + row.original.cedula,
+  //                   });
+  //                 },
+  //               });
+  //             }}
+  //           />
+  //           <IconButton
+  //             aria-label="Editar Material"
+  //             color="lendlab.light.red.400"
+  //             icon={<Icon as={FiEdit2} color="lendlab.yellow" />}
+  //             variant="ghost"
+  //             onClick={() => history.push("/dashboard/usuarios/" + row.original.cedula)}
+  //           />
+  //         </Stack>
+  //       );
+  //     },
+  //   },
+];
