c1e00870e460b7c8b9981ff563e1aaf47dcfb11d
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-11-17 12:37:42 -0300

    Change loading to Spinner

--- src/pages/Student/Home/PopularMaterials/index.jsx
+++ src/pages/Student/Home/PopularMaterials/index.jsx
@@ -1,4 +1,5 @@
 import { Stack } from "@chakra-ui/layout";
+import { Spinner } from "@chakra-ui/spinner";
 import MaterialItem from "@components/MaterialItem";
 import { usePopularMaterials } from "@graphql/materials/custom-hooks";
 import React from "react";
@@ -6,10 +7,15 @@ import React from "react";
 const PopularMaterials = () => {
   const { loading, data, error } = usePopularMaterials();
 
-  if (loading || !data) return "loading..";
+  if (loading || !data) return <Spinner />;
 
   return (
-    <Stack direction={{ md: "row", base: "column" }} justify="space-between" spacing={6} w="full">
+    <Stack
+      direction={{ md: "row", base: "column" }}
+      justify="space-between"
+      spacing={6}
+      w="full"
+    >
       {data.getPopularMaterials.map((material, i) => (
         <MaterialItem key={i} material={material} />
       ))}
