585b7d0d63010b6428820be20817a67d205bb04e
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-11-17 12:37:49 -0300

    Add LendList

--- src/pages/Student/My-Lends/index.jsx
+++ src/pages/Student/My-Lends/index.jsx
@@ -1,20 +1,11 @@
 import React from "react";
 import Dashboard from "@components/Dashboard";
-import { useMe } from "@graphql/auth/custom-hook";
-import { useUserLends } from "@graphql/lends/custom-hooks";
-import { Code } from "@chakra-ui/layout";
-import { Spinner } from "@chakra-ui/react";
+import { LendList } from "./LendList";
 
 const MyLends = () => {
-  const { data: dataMe } = useMe();
-
-  const { loading, data, error } = useUserLends(dataMe?.me.cedula);
-
-  if (loading || !data) return <Spinner />;
-
   return (
     <Dashboard hasNoActions title="Mis prestamos">
-      <Code>{JSON.stringify(data.getUserLends)}</Code>
+      <LendList />
     </Dashboard>
   );
 };
