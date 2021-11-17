a61549144b605b36f6a21525a34c157c684f8963
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-11-17 12:38:12 -0300

    Add ReservationList

--- src/pages/Student/My-Reservations/index.jsx
+++ src/pages/Student/My-Reservations/index.jsx
@@ -1,20 +1,12 @@
 import React from "react";
 import Dashboard from "@components/Dashboard";
-import { useUserReservations } from "@graphql/reservations/custom-hooks";
-import { Code } from "@chakra-ui/layout";
-import { useMe } from "@graphql/auth/custom-hook";
-import { Spinner } from "@chakra-ui/react";
+
+import { ReservationList } from "./ReservationList";
 
 const MyReservations = () => {
-  const { data: dataMe } = useMe();
-
-  const { loading, data, error } = useUserReservations(dataMe?.me.cedula);
-
-  if (loading || !data) return <Spinner />;
-
   return (
     <Dashboard hasNoActions title="Mis reservas">
-      <Code>{JSON.stringify(data.getUserReservations)}</Code>
+      <ReservationList />
     </Dashboard>
   );
 };
